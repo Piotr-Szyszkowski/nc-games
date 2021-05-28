4b2995fc3a5c191e1c0f272d58e7916db29e93b4
Piotr-Szyszkowski <peter.szyszkowski@protonmail.com>
2021-05-28 09:30:09 +0100

    started on sort by button

--- src/Components/Reviews.jsx
+++ src/Components/Reviews.jsx
@@ -13,11 +13,38 @@ const Reviews = () => {
     });
   }, [category]);
 
+  const handleSortByChange = (sortBy) => {
+    console.log(sortBy);
+  };
+
   return (
     <div className="Reviews__MainReviewsContainer">
       <header className="Reviews__Header">
         <h2>Reviews of {category} games:</h2>
       </header>
+      <section className="ReviewSorting">
+        <form input="submit" className="ReviewSorting__SortBy__Form">
+          <label
+            htmlFor="sortCategory"
+            className="ReviewSorting__SortBy__Form__Label"
+          >
+            Sort by:
+          </label>
+          <select
+            id="sortCategory"
+            name="sortCategory"
+            className="ReviewSorting__SortBy__Form__SelectField"
+            onChange={(event) => {
+              const chosenSortBy = event.target.value;
+              handleSortByChange(chosenSortBy);
+            }}
+          >
+            <option>Created at</option>
+            <option>Votes</option>
+            <option>Commentst</option>
+          </select>
+        </form>
+      </section>
       {reviewsList.map(
         ({ title, owner, review_id, created_at, comment_count, votes }) => {
           return (
