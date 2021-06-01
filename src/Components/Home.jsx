c361d15c65ff9c48756bf4848f212207c55bfcfe
Piotr-Szyszkowski <peter.szyszkowski@protonmail.com>
2021-06-01 11:04:13 +0100

    changed quote to J Romero

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,8 +4,9 @@ const Home = () => {
   return (
     <section className="Home">
       <h2 className="Home__Quote">
-        “Board Games FTW - because gaming platforms come and go, while floors &
-        tables stay pretty much the same...” - Paulo Coelho
+        “I think the reason that I like so many different games is because I
+        like the way my brain works when I'm playing games. It's more fun.” -
+        John Romero
       </h2>
     </section>
   );
