701334298bef2d274d7b6025339141575e3a3001
Peter-Szyszkowski <peterszyszkowski@24marketing.co.uk>
2021-09-30 19:45:36 +0100

    got rid of css in homepage

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Home = () => {
   return (
-    <section className="Home m-1 p-1 text-center font-light text-2xl text-gray-200 bg-red-900 bg-opacity-70">
+    <section className=" m-2 p-1 text-center font-light text-2xl text-red-700 bg-gray-300 bg-opacity-60 rounded-lg">
       <div className="bg-black h-full"></div>
-      <h2 className="Home__Quote ">
+      <h2 className="font-sriracha">
         “I think the reason that I like so many different games is because I
         like the way my brain works when I'm playing games. It's more fun.”
         <br /> - John Romero
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
         "odibee-sans": ['"Odibee Sans"', "cursive"],
         "squada-one": ['"Squada One"', "cursive"],
         inconsolata: ['"Inconsolata"', "monospace"],
+        sriracha: ['"Sriracha"', "cursive"],
       },
       backgroundImage: {
         "games-background-01": "url('./Images/games.png')",
